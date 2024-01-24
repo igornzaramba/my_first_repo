c77f8f498518181b56582e09ec5f9b117ebd204f
igornzaramba <157254338+igornzaramba@users.noreply.github.com>
2024-01-23 21:35:52 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+strat_year = input('Year of birth : ')
+end_year = input('Current year : ')
+
+strat_year = int(strat_year)
+end_year = int(end_year)
+
+age = end_year - strat_year
+
+print('Your age is', age, 'years old.')
