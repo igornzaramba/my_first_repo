98a90a4464cfb92b4c55e5e6c398d496d252c62a
igornzaramba <157254338+igornzaramba@users.noreply.github.com>
2024-01-24 20:31:01 -0500

    Add files via upload

---
+++ Area_of_circle.py
@@ -0,0 +1,11 @@
+# Import math Library
+import math
+
+radius=float(input('Enter the radiaus of a Circle:'))
+
+
+Area = (radius **2)*math.pi
+circonference = 2 * radius * math.pi
+
+
+print(f'The area of the circle is {Area} and the circonference is {circonference}.')
