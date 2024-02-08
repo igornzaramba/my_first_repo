b1c2e891cd99ea063457215eb2be1bc778db4498
igornzaramba <157254338+igornzaramba@users.noreply.github.com>
2024-02-07 22:39:57 -0500

    Update 2range.py

--- 2range.py
+++ 2range.py
@@ -1,25 +1,29 @@
 
 num = int(input("Enter a number: "))
+i=1
 
 if num < 2:
    print("Sorry, the provided number is smaller than 2")
 
 else:
-   print(2)
-   for ind in range(2, num +1):
-      if ind > 1:
-         for i in range(2,ind):
-            if (ind % i) == 0:
+   while i<num:
+
+      flag = False
+
+      if i == 1:
+         pass
+      elif i > 1:
+
+         for ind in range(2,i):
+            if (i % ind) == 0:
+               flag = True
                break
-            else:
-               print(ind)
-            break
-   
-   
+         if flag:
+            pass
+         else:
+            print(i, "is a prime number")
 
-                
-
-            
+      i+=1
             
 
                 
